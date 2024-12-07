f2561141e63ca5f583e066fb46129ecf027633ff
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2024-12-07 22:16:44 +0200

    feat: implemented item 10

--- main.cpp
+++ main.cpp
@@ -2,12 +2,28 @@
 
 using namespace std;
 
+class Widget {
+    private:
+        int value;
+
+    public:
+        Widget(int val = 0) : value(val) {}
+
+        Widget& operator=(const Widget& rhs) {
+            value = rhs.value;
+            return *this;
+        }
+
+        void display() const {
+            cout << "Value: " << value << endl;
+        }
+};
+
 int main() {
-    int x, y, z;
-    x = y = z = 15;
+    Widget w1(10), w2(20), w3;
 
-    cout << x << endl;
-
+    w3 = w2 = w1;
+    w3.display();
 
     return 0;
 }
